954e68c7bdbf129afc146d470ea029604c2162cd
sonicpro <sonic@ukrpost.net>
2018-06-21 18:53:40 +0300

    Dashboard component implemented.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StocksService, StockInterface } from '../../services/stocks.service'
 
 @Component({
   selector: 'dashboard',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public symbols: string[];
+  public stocks: StockInterface[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private service: StocksService) {
+    // Load default symbols from the service immediately.
+    this.symbols = service.get();
   }
 
+  ngOnInit() {
+    this.service.load(this.symbols).subscribe(stocks => this.stocks = stocks);
+  }
 }
